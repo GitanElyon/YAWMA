839b35adf294f51fb30e3540038d94d87034d8f5
GitanElyon <gitanmandellbalogh@gmail.com>
2024-10-12 23:53:38 -0400

    optimizing dropdown

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ const { invoke } = window.__TAURI__.core;
 // main.js
 import { initializeSearch } from './commands/SearchSystem.js';
 import { initializeTitlebarButtons } from './commands/TitlebarButtons.js';
+// import { initializeDropdowns } from './commands/DropdownLogic.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     initializeSearch();
     initializeTitlebarButtons();
+    // initializeDropdowns(); // Doesnt wanna work???
 });
 
